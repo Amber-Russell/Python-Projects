2d7fe152ca9d60d84a515141ce3901fa8cdb7e2d
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-07-22 20:36:29 -0700

    course exercises and game build

---
+++ PythonIfStatment.py
@@ -0,0 +1,25 @@
+
+
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key:
+        print('num1 is equal to 12 and they have the key')
+    else:
+        print('num1 is equal to 12 and they DO NOT have the key')
+elif num1 < 12:
+    print('num1 is less than 12')
+else:
+    print('num1 is not equal to 12')
+    
+
+age = 21
+
+if age <= 22:
+    print('You are old enough to drink')
+else:
+    print('You are not old enough to drink')
+    elif age == 21:
+        print('You are exactly old enough')
+        
---
+++ printNum.py
@@ -0,0 +1,10 @@
+
+import ourModule
+
+
+if __name__ == "__main__":
+    results = ourModule.getNumbers(5, 9)
+    print(results)
+
+a = ourModule.person1["age"]
+print(a)
