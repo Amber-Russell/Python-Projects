398e55156903ba96a80abe6ab9cb054f63ab09c7
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-08-12 16:53:20 -0700

    gui tkinter assignment

--- Tkinter_file_dir.py
+++ Tkinter_file_dir.py
@@ -1,26 +1,55 @@
 import tkinter
-from  tkinter import filedialog
-##import tkinter, filedialog
+from tkinter import *
+import tkinter.filedialog 
 import shutil
 import os
 
-#open directory
-root = tkinter.Tk()
-root.directory = filedialog.askdirectory()
-print (root.directory)
+#pick a folder from directory 
+def pick_sourcedir():
+    my_dir = tkinter.filedialog.askdirectory()
+    source_dir.delete(0, END)
+    source_dir.insert(0, my_dir)
 
-window_main = tkinter.Tk(className='Tkinter - TutorialKart', )
-window_main.geometry("400x200")
- 
-def submitFunction() :
-    print('Choose a file')
+#sends files from source directory to destination
+def pick_destination():
+    my_dir = tkinter.filedialog.askdirectory()
+    destination_dir.delete(0, END)
+    destination_dir.insert(0, my_dir)
+
+
+def file_move():
+    source = source_dir.get()
+    destination = destination_dir.get()
+    source_files = os.listdir(source)
+    for i in source_files:
+        shutil.move(source + '/' + i, destination)
+
+
+m = tkinter.Tk()
+m.minsize(750, 150)
+
+#Browse button and its geometrical location
+btn_Browse1 = Button(m, text="Browse...", padx=20, command=pick_sourcedir)
+btn_Browse1.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
+
+#entry for the source directory
+source_dir = Entry(m, width=100)
+source_dir.grid(row=0, column=1, padx=20, pady=(30, 0), sticky=E)
+
+#second button to browse to put the files in a new location
+btn_Browse2 = Button(m, text="Browse...", padx=20, command=pick_destination)
+btn_Browse2.grid(row=1, column=0, padx=(20, 10), pady=10)
+
+destination_dir = Entry(m, width=100)
+destination_dir.grid(row=1, column=1, padx=20, pady=(20, 0), sticky=E)
+
+
+btn_check = Button(text="Check for files...", pady=10, command=file_move)
+btn_check.grid(row=2, column=0, padx=(20, 10), pady=(0, 15))
 
 
 
-btn_Browse1 = Button(m, text="Browse...", padx=20, command=pickSourceDir)btn_Browse1.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
 
-btn_Browse2 = Button(m, text="Browse...", padx=20, command=pickDestDir)btn_Browse2.grid(row=1, column=0, padx=(20, 10), pady=10)
 
- 
-window_main.mainloop()
 
+m.mainloop()
