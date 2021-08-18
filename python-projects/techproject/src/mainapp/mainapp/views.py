ad0bdede96bf5dfe3b2242cd4120e392f992780c
Amber-Russell <83440345+Amber-Russell@users.noreply.github.com>
2021-08-17 17:39:28 -0700

    hello_world project

--- python-projects/techproject/src/mainapp/mainapp/views.py
+++ python-projects/techproject/src/mainapp/mainapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     user = request.user
-    context = (
+    context = {
         'user': user,
-    )
+    }
     return render(request, "home.html", context)
